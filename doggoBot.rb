3c99971847b1c0dc0484005102abad971424692e
Sylvain <sypsilon@hotmail.com>
2019-08-31 22:23:00 +0200

    Functionig bot

--- doggoBot.rb
+++ doggoBot.rb
@@ -9,22 +9,32 @@ bot = TelegramBot.new(token: token)
 bot.get_updates(fail_silently: true) do |message|
   puts "@#{message.from.username}: #{message.text}"
   command = message.get_command_for(bot)
-
     message.reply do |reply|
     case command
     when /start/i
-      reply.text = "I've got all the Dogs! Pawsome!"
-    else
+      reply.text = "Pawsome Dogs! Only for you!\n\nText \"breeds\" for all the puppers in our database.\nText your favourite breed name for extra cute pictures.\nText \"random\" for random doggo picture."
+    when /breed/i
+      result = ""
+      resString = JSON.parse(URI.parse("https://dog.ceo/api/breeds/list/all").read)
+      #p resString["message"].map { |e| e.keys}
+      result = resString["message"]
+      reply.text = "#{result}"
+    when /random/i
       result = ""
       resString = JSON.parse(URI.parse("https://dog.ceo/api/breeds/image/random").read)
-      if resString.empty?
-        reply.text = "Breed #{message.text} doesn't exist"
+      result = resString["message"]
+      reply.text = "#{result}"
+    else
+      result = ""
+      begin
+        resString = JSON.parse(URI.parse("https://dog.ceo/api/breed/#{message.text.downcase}/images/random").read)
+      rescue StandardError => e
+        reply.text = "We couldn't find anything for #{message.text} :("
         puts "sending #{reply.text.inspect} to @#{message.from.username}"
         reply.send_with(bot)
         break
       end
-      result = resString["message"]
-      reply.text = "#{result}"
+      reply.text = "#{resString['message']}"
     end
     puts "sending #{reply.text.inspect} to @#{message.from.username}"
     reply.send_with(bot)
